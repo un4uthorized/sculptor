c631ebfdbeab8aac55a8b94d2832271de18b0a80
devneto <miguel.neto320@gmail.com>
2023-02-27 22:21:34 -0300

    chore: add test support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+  },
 })
